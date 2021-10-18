9fbdc19c44fbb6c452f323ca4540da4e2b5a2d01
root <root@control-plane.minikube.internal>
2021-10-18 13:11:57 +0000

    No longer using init container

--- Kubernetes/mgwebentry/entrypoint.sh
+++ Kubernetes/mgwebentry/entrypoint.sh
@@ -3,4 +3,9 @@ service ssh start
 cd /usr/local/YottaDB-dashboard/glbview
 /usr/local/YottaDB-dashboard/glbview/globview.sh start
 /usr/local/bin/start.sh
+if ! test -f /opt/yottadb/yottadb.gld
+then
+ /usr/local/bin/dbprov.sh
+ (echo "D ^%GI";echo "/home/footie/Football.go";echo "") | ydb
+fi
 cd /opt/mgweb && /opt/mgweb/start
