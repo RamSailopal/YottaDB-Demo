ea73f83b691bab4ed27952ea1548ea2f813b872d
RamSailopal <thelinuxguys@yahoo.com>
2021-10-20 17:27:55 +0100

    Needed to add an asterix

--- Kubernetes/ideentry/entrypoint.sh
+++ Kubernetes/ideentry/entrypoint.sh
@@ -2,8 +2,8 @@
 mkdir /root/.ssh
 cp -Rf /home/git/YottaDB-Demo/.ssh/* /root/.ssh
 mkdir /home/theia/.theia
-cp -Rf /home/git/YottaDB-Demo/Kubernetes/.theia /home/theia/.theia
+cp -Rf /home/git/YottaDB-Demo/Kubernetes/.theia/* /home/theia/.theia
 mkdir /home/yottadb-settings
-cp -Rf /home/git/YottaDB-Demo/project /home/yottadb-settings
+cp -Rf /home/git/YottaDB-Demo/project/* /home/yottadb-settings
 cd /home/theia
 node /home/theia/src-gen/backend/main.js /home/project --hostname=0.0.0.0
