e001c640d3c947d091ea25da7131a087d81c7467
RamSailopal <thelinuxguys@yahoo.com>
2021-10-21 15:37:58 +0100

    Added a halt to the end of the DB import commands

--- Kubernetes/mgwebentry/initentry.sh
+++ Kubernetes/mgwebentry/initentry.sh
@@ -5,4 +5,4 @@ export ydb_gbldir=/opt/yottadb/yottadb.gld
 /usr/local/lib/yottadb/r130/mupip rundown -region default
 /usr/local/lib/yottadb/r130/mupip set -key_size=1019 -region default
 /usr/local/lib/yottadb/r130/mupip set -record_size=1048576 -region default
-(echo "D ^%GI";echo "/home/git/YottaDB-Demo/EnglishFootball/Football.go";echo "") | ydb
+(echo "D ^%GI";echo "/home/git/YottaDB-Demo/EnglishFootball/Football.go";echo "";echo "H") | ydb
