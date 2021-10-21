8bed8a3adfac68427a22cd79688c03a435b9af46
RamSailopal <thelinuxguys@yahoo.com>
2021-10-21 14:40:25 +0100

    Should be Demo and not dashboard

--- Kubernetes/metaentry/entrypoint.sh
+++ Kubernetes/metaentry/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /home/metabase
-cp -f /home/git/YottaDB-Dashboard/metabase/* /home/metabase
+cp -f /home/git/YottaDB-Demo/metabase/* /home/metabase
 echo "$IPADD demo_yottamgweb_1" >> /etc/hosts
 /app/bin/start
