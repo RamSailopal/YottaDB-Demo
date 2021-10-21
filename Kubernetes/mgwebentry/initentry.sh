6f1d9ddacb343c404ebdbde26e2636a22acd21f7
RamSailopal <thelinuxguys@yahoo.com>
2021-10-21 15:54:06 +0100

    Added a db rndown to the end of the init script

--- Kubernetes/mgwebentry/initentry.sh
+++ Kubernetes/mgwebentry/initentry.sh
@@ -6,3 +6,4 @@ export ydb_gbldir=/opt/yottadb/yottadb.gld
 /usr/local/lib/yottadb/r130/mupip set -key_size=1019 -region default
 /usr/local/lib/yottadb/r130/mupip set -record_size=1048576 -region default
 (echo "D ^%GI";echo "/home/git/YottaDB-Demo/EnglishFootball/Football.go";echo "";echo "H") | ydb
+/usr/local/lib/yottadb/r130/mupip rundown -region default
