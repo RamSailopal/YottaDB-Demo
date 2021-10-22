db8b45c8674fb529c6fb4bda8ae4e54978b095a0
RamSailopal <thelinuxguys@yahoo.com>
2021-10-22 13:12:05 +0100

    Changing the string to grep on

--- Kubernetes/mgwebentry/start.sh
+++ Kubernetes/mgwebentry/start.sh
@@ -32,7 +32,7 @@ fi
 export ydb_gbldir=/opt/yottadb/yottadb.gld
 out=$(ydb <<< 'D start^%zmgwebUtils' 2>&1)
 echo "$out"
-if grep -q "Must be rundown" <<< $ram
+if grep -q "%YDB-E" <<< $ram
 then
 	pid=$(lsof | grep mgweb.dat | awk '{ print $2 }')
 	if [[ "$pid" != "" ]]
